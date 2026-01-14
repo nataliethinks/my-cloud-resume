41a270fdc11d12b8fc2f336c026048701f52b326
Natalie <kelsikeichiu@gmail.com>
2026-01-14 00:58:41 -0500

    fix: set router basename for github pages

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-// import Resume from "./Resume";
 import { BrowserRouter } from "react-router-dom";
+// import Resume from "./Resume";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter basename="/my-cloud-resume">
     <App />
