1878eab8060c2fb4b3459d791ae53f19cbb7a184
Natalie <kelsikeichiu@gmail.com>
2026-01-14 01:22:24 -0500

    fix: use a single router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import "./styles/site.css";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
 import ResumePage from "./pages/ResumePage";
 import BlogPage from "./pages/BlogPage";
 import BlogPostPage from "./pages/BlogPostPage";
-import "./App.css";
-
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <>
       <nav className="nav-right">
         <Link to="/" className="nav-btn">Resume</Link>
         <Link to="/blog" className="nav-btn">Blog</Link>
       </nav>
 
       <Routes>
-        <Route path="*" element={<ResumePage />} />
+        <Route path="/" element={<ResumePage />} />
         <Route path="/blog" element={<BlogPage />} />
-         <Route path="/blog/:slug" element={<BlogPostPage />} />
+        <Route path="/blog/:slug" element={<BlogPostPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-// import Resume from "./Resume";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/my-cloud-resume">
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
